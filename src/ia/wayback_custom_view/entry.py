15d3c1587c3c9c5bb69e01c4edb7f5e14f341887
Kenji Nagahashi <kenji@archive.org>
2025-03-13 01:45:27 +0000

    add entry_point config for "gwb.custom_view" - now it's possible to expose python objects to the custom view template.

---
+++ src/ia/wayback_custom_view/entry.py
@@ -0,0 +1,18 @@
+# defines entry points
+import re
+
+# def hello_world():
+#     return 'hello_world!'
+
+gwb_custom_view = {
+    'urlkey_match': [
+        re.compile(r'com,twitter\)/[^/]+/status/\d+$')
+    ],
+    'view':  {
+        'template': 'jsontweet',
+        'tvars': {
+            # can expose Python objects to template
+            # 'hello_world': hello_world
+        }
+    }
+}
