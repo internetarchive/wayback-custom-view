3826afffd5ca8010c6aa543d52f70edc3971266a
Dan Visel <danvisel@archive.org>
2025-03-13 12:52:19 +0100

    took out make replay image

--- src/ia/wayback_custom_view/entry.py
+++ src/ia/wayback_custom_view/entry.py
@@ -9,21 +9,21 @@ def get_timestamp(wayback_full_url):
     """get timestamp from request"""
     return wayback_full_url.split('/')[2]
 
-def make_replay_image_url(absurl, timestamp, flags=None):
-    """make playback URL for the target URL `url` at time `timestamp`,
-    and `flags`. This version can only generate absolute URL (no `style`
-    argument.) for production wayback (web.archive.org)
+# def make_replay_image_url(absurl, timestamp="", flags=None):
+#     """make playback URL for the target URL `url` at time `timestamp`,
+#     and `flags`. This version can only generate absolute URL (no `style`
+#     argument.) for production wayback (web.archive.org)
 
-    Also: strip extension off of URL, add "?name=orig&format={extension}
-    """
-    base_url = 'https://web.archive.org/web'
-    stripped_url = absurl.rsplit('.',1)[0]
-    extension = absurl.rsplit('.',1)[1]
-    # timestamp = get_timestamp(capture_url)
-    # timestamp = timestamp.decode('latin1')
-    if flags:
-        timestamp += ''.join(f'{fl}_' for fl in flags)
-    return f'{base_url}/{timestamp}/{stripped_url}?name=orig&format={extension}'
+#     Also: strip extension off of URL, add "?name=orig&format={extension}
+#     """
+#     base_url = 'https://web.archive.org/web'
+#     stripped_url = absurl.rsplit('.',1)[0]
+#     extension = absurl.rsplit('.',1)[1]
+#     # timestamp = get_timestamp(capture_url)
+#     # timestamp = timestamp.decode('latin1')
+#     if flags:
+#         timestamp += ''.join(f'{fl}_' for fl in flags)
+#     return f'{base_url}/{timestamp}/{stripped_url}?name=orig&format={extension}'
 
 gwb_custom_view = {
     'urlkey_match': [
@@ -35,7 +35,7 @@ gwb_custom_view = {
             # can expose Python objects to template
             'hello_world': hello_world,
             'get_timestamp': get_timestamp,
-            'make_replay_image_url': make_replay_image_url
+            # 'make_replay_image_url': make_replay_image_url
         }
     }
 }
