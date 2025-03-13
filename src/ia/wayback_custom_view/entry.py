3f7fed368ca7f33e27e16937ec8ae0280921988e
Dan Visel <danvisel@archive.org>
2025-03-13 13:31:54 +0100

    changed where i was looking

--- src/ia/wayback_custom_view/entry.py
+++ src/ia/wayback_custom_view/entry.py
@@ -2,28 +2,28 @@
 import re
 
 def hello_world():
-    """simple function to test if Python functions can be exposed to templates"""
+    # """simple function to test if Python functions can be exposed to templates"""
     return 'hello_world!'
 
-# def get_timestamp(wayback_full_url):
-#     """get timestamp from request"""
-#     return wayback_full_url.split('/')[2]
+def get_timestamp(wayback_full_url="/////"):
+    """get timestamp from request"""
+    return wayback_full_url.split('/')[4]
 
-# def make_replay_image_url(absurl, timestamp="", flags=None):
-#     """make playback URL for the target URL `url` at time `timestamp`,
-#     and `flags`. This version can only generate absolute URL (no `style`
-#     argument.) for production wayback (web.archive.org)
+def make_replay_image_url(absurl, timestamp="", flags=None):
+    """make playback URL for the target URL `url` at time `timestamp`,
+    and `flags`. This version can only generate absolute URL (no `style`
+    argument.) for production wayback (web.archive.org)
 
-#     Also: strip extension off of URL, add "?name=orig&format={extension}
-#     """
-#     base_url = 'https://web.archive.org/web'
-#     stripped_url = absurl.rsplit('.',1)[0]
-#     extension = absurl.rsplit('.',1)[1]
-#     # timestamp = get_timestamp(capture_url)
-#     # timestamp = timestamp.decode('latin1')
-#     if flags:
-#         timestamp += ''.join(f'{fl}_' for fl in flags)
-#     return f'{base_url}/{timestamp}/{stripped_url}?name=orig&format={extension}'
+    Also: strip extension off of URL, add "?name=orig&format={extension}
+    """
+    base_url = 'https://web.archive.org/web'
+    stripped_url = absurl.rsplit('.',1)[0]
+    extension = absurl.rsplit('.',1)[1]
+    # timestamp = get_timestamp(capture_url)
+    # timestamp = timestamp.decode('latin1')
+    if flags:
+        timestamp += ''.join(f'{fl}_' for fl in flags)
+    return f'{base_url}/{timestamp}/{stripped_url}?name=orig&format={extension}'
 
 gwb_custom_view = {
     'urlkey_match': [
@@ -33,9 +33,9 @@ gwb_custom_view = {
         'template': 'jsontweet',
         'tvars': {
             # can expose Python objects to template
-            'hello_world': hello_world
-            # 'get_timestamp': get_timestamp,
-            # 'make_replay_image_url': make_replay_image_url
+            'hello_world': hello_world,
+            'get_timestamp': get_timestamp,
+            'make_replay_image_url': make_replay_image_url
         }
     }
 }
